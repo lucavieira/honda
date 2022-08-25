8240cf09211870f1c684b89c6218ae36798557e6
lucavieira <lukasveras14@gmail.com>
2022-08-25 20:08:26 -0300

    Finalização, menu lateral que altera o modelo das motos e ajustes no css.

---
+++ src/js/controller.js
@@ -0,0 +1,27 @@
+let changeRed = document.getElementById('change-red')
+let changeBlack = document.getElementById('change-black')
+let changeBlue = document.getElementById('change-blue')
+
+let current_image = document.getElementById('moto')
+
+function modifyImage(color) {
+    current_image.src = `../src/images/cb_twister_${color.toLowerCase()}.png`
+
+    if(color == 'Red') {
+        changeRed.style.backgroundColor = '#C60000'
+        changeBlack.style.backgroundColor = '#fff'
+        changeBlue.style.backgroundColor = '#fff'
+    } else if (color == 'Black') {
+        changeRed.style.backgroundColor = '#fff'
+        changeBlack.style.backgroundColor = '#C60000'
+        changeBlue.style.backgroundColor = '#fff'
+    } else {
+        changeRed.style.backgroundColor = '#fff'
+        changeBlack.style.backgroundColor = '#fff'
+        changeBlue.style.backgroundColor = '#C60000'
+    }
+}
+
+changeRed.addEventListener("click", () => modifyImage('Red'))
+changeBlack.addEventListener("click", () => modifyImage('Black'))
+changeBlue.addEventListener("click", () => modifyImage('Blue'))
